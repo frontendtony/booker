e03856c85ba24ffec511388faf374767dd5684b8
Blessing <56820716+blessing986@users.noreply.github.com>
2025-05-28 03:27:59 +0100

    feat: implemented 3-column grid layout with fixed sidebar. (#2)
    * feat: implemented 3-column grid layout with fixed sidebar and max width of 1440px.
    
    * refactor: simplify layout by removing max-width and merging grid container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div className="font-3xl font-bold underline">Hello world!!</div>;
+  return (
+    <div className="grid min-h-screen grid-cols-[280px_1fr_1fr]">
+      <div className="bg-white p-4">
+        <h2 className="mb-2 font-bold">Sidebar</h2>
+      </div>
+
+      <div className="bg-gray-100 p-4">
+        <h2 className="mb-2 font-bold">Main Content</h2>
+      </div>
+
+      <div className="bg-white p-4">
+        <h2 className="mb-2 font-bold">Main Content</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
