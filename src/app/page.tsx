da9bc5b8dcc52333ccae260d84f00fb78b9a699f
Blessing U <ubiomorblessing@gmail.com>
2025-06-20 16:07:15 +0100

    chore: improve home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import Sidebar from "@/components/sidebar";
 
 const Home = () => {
   return (
-    <div className="grid min-h-screen grid-cols-[280px_1fr_1fr]">
-      <div className="hide-scrollbar h-screen overflow-y-auto bg-white pt-4 pl-6">
+    <div className="grid h-screen grid-cols-[280px_1fr_1fr]">
+      <div className="hide-scrollbar overflow-y-auto bg-white p-6">
         <Sidebar />
       </div>
 
-      <div className="hide-scrollbar h-screen overflow-y-auto bg-[#FAFAFC] p-6">
+      <div className="hide-scrollbar overflow-y-auto bg-[#FAFAFC] p-6">
         <CentreLayoutPopulated />
       </div>
 
-      <div className="hide-scrollbar h-screen overflow-y-auto bg-white p-4">
+      <div className="hide-scrollbar overflow-y-auto bg-white p-4">
         <LargeCalendarPopulated />
       </div>
     </div>
